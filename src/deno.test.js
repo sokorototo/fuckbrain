4b00c18da50281edfc17269a6f82d03852ad43b2
sokorototo <nyachiengatoto@gmail.com>
2021-08-03 16:05:03 +0300

    Fixed Deno test script on Linux

--- src/deno.test.js
+++ src/deno.test.js
@@ -1,30 +1,33 @@
 "use strict";
 import Machine from "https://cdn.jsdelivr.net/npm/fuckbrain/build/fuckbrain.min.mjs";
-console.log("Running Deno tests: \n");
+console.log("\n================= Running Deno tests ==================== \n");
 
-const readFileSync = (filename) => Deno.readTextFile(filename).then( data => { return { data ,name: filename } });
-let machine = new Machine({ cellSize: 3 });
+const readFileSync = (filename) => Deno.readTextFile(`${Deno.cwd()}/${filename}`).then(data => { return { data, name: filename } });
+const machine = new Machine({ cellSize: 3 });
 
-let promises = [
-    "examples/bench/skiploop.bf",
-    "examples/oobrain.bf",
-    "examples/asciiart/chess.bf",
-    "examples/password-vault.bf",
-    "examples/reverse.bf",
-    "examples/utm.bf",
-    "examples/jabh.bf",
-    "examples/cell-size-5.bf",
-    "examples/passtest.bf"
-]
-.map( readFileSync );
+const promises =
+	[
+		"examples/cell-size-1.bf",
+		"examples/bench/prttab.bf",
+		"examples/bench/skiploop.bf",
+		"examples/bottles-3.bf",
+		"examples/oobrain.bf",
+		"examples/asciiart/chess.bf",
+		"examples/196.bf",
+		"examples/reverse.bf",
+		"examples/utm.bf",
+		"examples/jabh.bf",
+		"examples/torture.bf"
+	]
+		.map(readFileSync);
 
 Promise.all(promises).then(files => {
-    files.map(file => {
-        let then = Date.now();
-        machine.run(file.data, Machine.StringInputGenerator("b1b1bbb1c1c11111d"), {
-            complete( output ){
-                console.log(`\n[FILENAME] => ${file.name},\n[OUTPUT BYTES] => ${output.length * 8},\nExecution took: ${Date.now() - then}ms.\n`);
-            }
-        })
-    });
+	files.map(file => {
+		const then = Date.now();
+		machine.run(file.data, Machine.StringInputGenerator("132\n"), {
+			complete(output) {
+				console.log(`\n[FILENAME] => ${file.name},\n[OUTPUT BYTES] => ${output.length * 8},\nExecution took: ${Date.now() - then}ms.`);
+			}
+		})
+	});
 })
---
+++ src/fuckbrain.test.mjs
@@ -0,0 +1,32 @@
+"use strict";
+console.log("\n================= Running Node.js tests ==================== \n");
+
+import { readFileSync } from "fs";
+import Machine from "../build/fuckbrain.min.mjs";
+
+let machine = new Machine({ cellSize: 3 });
+
+[
+	"examples/cell-size-1.bf",
+	"examples/bench/prttab.bf",
+	"examples/bench/skiploop.bf",
+	"examples/bottles-3.bf",
+	"examples/oobrain.bf",
+	"examples/asciiart/chess.bf",
+	"examples/196.bf",
+	"examples/reverse.bf",
+	"examples/utm.bf",
+	"examples/jabh.bf",
+	"examples/torture.bf"
+]
+	.map(file => {
+		console.time(`[PERFORMANCE] => ${file}, took`);
+		machine.run(readFileSync(file, "utf8"), Machine.StringInputGenerator("132\n"), {
+			complete(output) {
+				console.log(`\n[FILENAME] => ${file},\n[OUTPUT BYTES] => ${output.length * 8}\n`);
+				console.timeEnd(`[PERFORMANCE] => ${file}, took`);
+			}
+		})
+	});
+
+console.log("All tests passed!");
