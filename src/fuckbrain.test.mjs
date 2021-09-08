1302f0c73335360c520680d5dae53445c0e9bdb2
sokorototo <nyachiengatoto@gmail.com>
2021-09-09 02:06:16 +0300

    Refactor

--- src/fuckbrain.test.mjs
+++ src/fuckbrain.test.mjs
@@ -19,14 +19,14 @@ let machine = new Machine({ cellSize: 3 });
 	"examples/jabh.bf",
 	"examples/torture.bf"
 ]
-	.map(file => {
-		console.time(`[PERFORMANCE] => ${file}, took`);
-		machine.run(readFileSync(file, "utf8"), Machine.StringInputGenerator("132\n"), {
-			complete(output) {
-				console.log(`\n[FILENAME] => ${file},\n[OUTPUT BYTES] => ${output.length * 8}\n`);
-				console.timeEnd(`[PERFORMANCE] => ${file}, took`);
-			}
-		})
-	});
+.map(file => {
+	console.time(`[PERFORMANCE] => ${file}, took`);
+	machine.run(readFileSync(file, "utf8"), Machine.StringInputGenerator("132\n"), {
+		complete(output) {
+			console.log(`\n[FILENAME] => ${file},\n[OUTPUT BYTES] => ${output.length * 8}\n`);
+			console.timeEnd(`[PERFORMANCE] => ${file}, took`);
+		}
+	})
+});
 
 console.log("All tests passed!");
