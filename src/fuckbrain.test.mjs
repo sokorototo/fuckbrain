5500e9f2d83a7ef98d1e6e2f3ca34484c7ce517f
sokorototo <nyachiengatoto@gmail.com>
2021-11-04 21:08:12 +0300

    Better tests , better tastes

--- src/fuckbrain.test.mjs
+++ src/fuckbrain.test.mjs
@@ -3,30 +3,37 @@ console.log("\n================= Running Node.js tests ==================== \n")
 
 import { readFileSync } from "fs";
 import Machine from "../build/fuckbrain.min.mjs";
+import assert from "assert";
 
-let machine = new Machine({ cellSize: 3 });
+let machine = new Machine();
 
-[
-	"examples/cell-size-1.bf",
-	"examples/bench/prttab.bf",
-	"examples/bench/skiploop.bf",
-	"examples/bottles-3.bf",
-	"examples/oobrain.bf",
-	"examples/asciiart/chess.bf",
-	"examples/196.bf",
-	"examples/reverse.bf",
-	"examples/utm.bf",
-	"examples/jabh.bf",
-	"examples/torture.bf"
-]
-	.map(file => {
-		console.time(`[PERFORMANCE] => ${file}, took`);
-		machine.run(readFileSync(file, "utf8"), Machine.StringInputGenerator("132\n"), {
-			complete(output) {
-				console.log(`\n[FILENAME] => ${file},\n[OUTPUT BYTES] => ${output.length * 8}`);
-				console.timeEnd(`[PERFORMANCE] => ${file}, took`);
-			}
-		})
-	});
+let inputs = {
+	"examples/cell-size-1.bf": 264,
+	"examples/bench/prttab.bf": 5208,
+	"examples/bench/skiploop.bf": 24,
+	"examples/bottles-3.bf": 90872,
+	"examples/oobrain.bf": 1840,
+	"examples/asciiart/chess.bf": 21648,
+	"examples/196.bf": 368,
+	"examples/reverse.bf": 32,
+	"examples/utm.bf": 8,
+	"examples/jabh.bf": 240,
+	"examples/torture.bf": 136
+};
 
-console.log("All tests passed!");
+
+Object.keys(inputs).forEach(file => {
+	console.time(`[PERFORMANCE] => ${file}, took`);
+	machine.run(readFileSync(file, "utf8"), Machine.StringInputGenerator("132\n"),
+	{
+		complete(output) {
+			const outputSize = output.length * 8;
+			assert(outputSize == inputs[file], `[FILE: ${file}] output an invalid number of bytes: ${outputSize} != ${inputs[file]}`);
+
+			console.log(`\n[FILENAME] => ${file},\n[OUTPUT BYTES] => ${outputSize}`);
+			console.timeEnd(`[PERFORMANCE] => ${file}, took`);
+		}
+	})
+});
+
+console.log("\n================= All tests passed! ==================== \n");
